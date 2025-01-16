ec4164562e3da05166751d78733231a7c864d137
Ankrit Jarngal <91985@LAPTOP-125398.ONGC.ONGCGroup.co.in>
2025-01-17 03:32:03 +0530

    message  working

--- portfolio/pages/Footer.jsx
+++ portfolio/pages/Footer.jsx
@@ -1,4 +1,24 @@
 import { useState } from "react"
+import { KEY } from "../KEY";
+import { BIN_ID } from "../KEY";
+function handle(msg){
+    const binId = BIN_ID; 
+    const apiUrl = `https://api.jsonbin.io/v3/b/${binId}`;
+   const newData = msg;  
+   
+fetch(apiUrl, {
+ method: 'PUT',
+ headers: {
+   'Content-Type': 'application/json',
+   'X-Master-Key': KEY,  
+ },
+ body: JSON.stringify(newData), 
+})
+ .then(response => response.json())  
+ .then(data => console.log('Updated Bin Data:', data))
+ .catch(error => console.error('Error:', error));
+
+}
 
 export function Footer(){
     const[msg,setMsg]=useState("");
@@ -10,12 +30,14 @@ export function Footer(){
                 <input  className="mt-1 w-full p-2 rounded-lg bg-gray-800 text-white border border-gray-700 focus:ring-blue-500 focus:border-blue-500 shadow-sm transition-all duration-300"type="text" placeholder="  Your Suggestions Here" onChange={(e)=>{
                 setMsg(e.target.value);
             }}/>
-            <button className=" text-[#D1D5DB]  mt-1 w-full p-2 rounded-lg bg-gray-800 border border-gray-700 focus:border-blue-500 shadow-sm transition-all duration-300 ">Send</button>
+            <button className=" text-[#D1D5DB]  mt-1 w-full p-2 rounded-lg bg-gray-800 border border-gray-700 focus:border-blue-500 shadow-sm transition-all duration-300 " onClick={()=>{handle(msg)}}>Send</button>
             </div>
             <p className=" text-gray-300 py-1">Made by Ankrit 🐒</p>
 
 
         </div>
     )
+   
+
 
 }
