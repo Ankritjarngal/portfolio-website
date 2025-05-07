4ede7a05881eaf15b047349831916bdec2815cab
ankrit <jarngalankrit@gmail.com>
2025-05-07 19:14:16 +0530

    minor

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -13,7 +13,7 @@ export function Projects() {
         <div className="transform hover:scale-[1.02] transition-all">
   <PerProject
     title="Smart Resume Analyzer: Score, Compare & Matching Internships"
-    text="AI-powered Resume Evaluator that extracts key details using NLP and NER. Processes PDFs, scores resumes on predefined criteria, and integrates vector-based search with Pinecone for similarity matching. Includes a search feature for finding similar resumes based on skills, education, and experience. Secure storage via Cloudinary. Features automated web scraping to fetch relevant Internshala internships based on extracted skills, allowing users to browse and apply directly."
+    text="AI-powered Resume Evaluator that extracts key details using NLP and NER. Processes PDFs, scores resumes on predefined criteria Using AI, and integrates vector-based search with Pinecone for similarity matching for similarity searching. Includes a search feature for finding similar resumes based on skills, education, and experience using the vector database. Secure storage via Google Drive API. Features automated web scraping to fetch relevant Internshala internships based on extracted skills, allowing users to browse and apply directly."
     skills={[
       "NLP",
       "NER",
